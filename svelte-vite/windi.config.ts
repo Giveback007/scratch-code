27fbb016c0c2c2f8dc2e8713da5746e6bbd1acad
Dovydas Stirpeika <Dovy91@gmail.com>
2022-07-25 11:54:33 -0400

    git init

---
+++ svelte-vite/windi.config.ts
@@ -0,0 +1,8 @@
+// https://github.com/windicss/vite-plugin-windicss
+import { defineConfig } from 'windicss/helpers';
+// import formsPlugin from 'windicss/plugin/forms';
+
+export default defineConfig({
+    // plugins: [formsPlugin],
+    attributify: true,
+});
